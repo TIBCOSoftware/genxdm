8c623feea1cbd8851e3d063510e743bee85d664e
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2010-12-16 21:07:16 +0000

    removed remaining axiom tests.  i/o and xpath tests also conflicted with the use of junit 4.  added first two new contract-based conformance tests; axiom passes them (some bullet-proofing had to be added; note that the 'reset()' method was *vastly* incomplete and unsafe in its earlier form, and we can speculate that the same is true for builders in other bridges).  the builder test base is incomplete; either it will be completed next, or we'll work on the model test base, which is sort of pride-of-place, in many ways.

--- project/bridge/bridgetest/src/main/java/org/genxdm/bridgetest/base/io/BuilderTestBase.java
+++ project/bridge/bridgetest/src/main/java/org/genxdm/bridgetest/base/io/BuilderTestBase.java
@@ -19,7 +19,11 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import javax.xml.XMLConstants;
+
+import org.genxdm.DtdAttributeKind;
 import org.genxdm.base.ProcessingContext;
 import org.genxdm.base.io.FragmentBuilder;
 
@@ -72,25 +76,132 @@ public abstract class BuilderTestBase<N>
     public void illFormed()
     {
         // test various ways of supplying bad data
-        // startElement, text, attribute (sub comment|pi for text; sub ns for attr)
+        ProcessingContext<N> context = newProcessingContext();
+        
+        FragmentBuilder<N> builder = context.newFragmentBuilder();
+        
+        assertNotNull(builder); // never null
+
+        // contract for attribute and namespace seems to be:
+        // if there's an element open, it goes there, even if we've
+        // already started putting child content in.
+        // comment this out for now, and think about it a bit.
+//        // TODO: vary by substituting comment, pi for text.
+//        // startElement, text, attribute (sub comment|pi for text; sub ns for attr)
+//        builder.startElement(XMLConstants.NULL_NS_URI, "element", XMLConstants.DEFAULT_NS_PREFIX);
+//        builder.text("text");
+//        try
+//        {
+//            builder.attribute(XMLConstants.NULL_NS_URI, "attr", XMLConstants.DEFAULT_NS_PREFIX, "value", DtdAttributeKind.CDATA);
+//            fail("created attribute sibling of text node inside element");
+//        }
+//        catch (RuntimeException ge)
+//        {
+//            // expected: you can't create an attribute as the sibling of a text node
+//        }
+//        try
+//        {
+//            builder.namespace("xyzzy", "http://great.underground.empire/");
+//            fail("created namespace sibling of text node inside element");
+//        }
+//        catch (RuntimeException ge)
+//        {
+//            // expected; can't create namespace sibling of text node inside element.
+//        }
+        
+        builder.reset();
         // startdocument, startElement, endDocument
+        builder.startDocument(null, null);
+        builder.startElement(XMLConstants.NULL_NS_URI, "element", XMLConstants.DEFAULT_NS_PREFIX);
+        try
+        {
+            builder.endDocument();
+            fail("ended document with open element");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected; can't end the document when there are elements open
+        }
+        
+        builder.reset();
         // startdocument, text (not just whitespace)
+        builder.startDocument(null, null);
+        try
+        {
+            builder.text("text is not allowed in the prolog");
+            fail("permitted non-whitespace characters in the prolog");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected: can't put non-whitespace text in the prolog
+        }
+
+        builder.reset();
         // startdocument, startelement, endelement, text (not whitespace)
+        builder.startDocument(null, null);
+        builder.startElement(XMLConstants.NULL_NS_URI, "element", XMLConstants.DEFAULT_NS_PREFIX);
+        builder.endElement();
+        try
+        {
+            builder.text("text is not allowed in the epilog");
+            fail("permitted non-whitespace characters in the epilog");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected: can't put non-whitespace text in the epilog
+        }
+        
+        builder.reset();
+        // TODO: same thing for document inside document.
         // startelement, startdocument (or startdocument after *anything* but enddocument)
+        builder.startElement(XMLConstants.NULL_NS_URI, "element", XMLConstants.DEFAULT_NS_PREFIX);
+        try
+        {
+            builder.startDocument(null, null);
+            fail("document contained in element");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected: can't put a document inside an element
+        }
+        
+        builder.reset();
         // endelement with no corresponding startelement
+        try
+        {
+            builder.endElement();
+            fail("permitted the end of a non-existent element");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected: can't end an element that isn't started
+        }
+        
+        builder.reset();
         // enddocument with no corresponding startdocument
-        // sibling text?
+        try
+        {
+            builder.endDocument();
+            fail("permitted the end of a non-existent document");
+        }
+        catch (RuntimeException ge)
+        {
+            // expected: can't end a document that isn't started
+        }
+        
+        builder.reset();
+        // sibling text
+        // hmmmm.  probably can't test this here.
+        // it's *legal* to have multiple calls to builder.text().
+        // it just isn't legal to have multiple text nodes in a normalized model.
     }
     
-    private void createSequence(FragmentBuilder<N> builder)
+    private void areTheseBitsLegal()
     {
-        builder.reset();
-        
-        // create something *weird*.  there are several possibilities,
-        // and the question is, what's legal?
-        
         // a list of attributes.  Note: it isn't reasonable to have attributes mixed
-        // with anything else.
+        // with anything else ... except, if it's reasonable to have just
+        // a list of attributes, then it ought to be possible to concatenate
+        // that list with any other valid list of nodes.
         
         // a preliminary list of namespaces, followed by several elements
         // which may have complex content.  this is the "fragment" model
