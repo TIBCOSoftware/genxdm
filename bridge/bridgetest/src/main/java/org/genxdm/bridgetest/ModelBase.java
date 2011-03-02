bb1eb7f161c7748477cd3ae59d3a4fcbceddd308
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-03-02 19:10:35 +0000

    okay, add checking of attributes and namespaces.  note that there's an oddity in the axiom bridge, where we use 'finally' in streaming, that can make debugging much more difficult than it ought to be. also, the createnamespace method has been removed from nodefactory (because a namespace need not be a node), in favor of the mutablemodel/mutablecursor insertnamespace method (note some legacy in cx, though ... typical. *sigh*).

--- bridge/bridgetest/src/main/java/org/genxdm/bridgetest/ModelBase.java
+++ bridge/bridgetest/src/main/java/org/genxdm/bridgetest/ModelBase.java
@@ -40,6 +40,8 @@ public abstract class ModelBase<N>
         Events<N> matcher = new Events<N>(builder);
         if (!context.isSupported(Feature.DOCUMENT_URI))
             matcher.ignoreDocumentURI();
+        if (!context.isSupported(Feature.NAMESPACE_AXIS))
+            matcher.ignoreExtraNamespaceDeclarations();
         matcher.record();
         N doc = createComplexTestDocument(matcher);
         assertNotNull(doc);
