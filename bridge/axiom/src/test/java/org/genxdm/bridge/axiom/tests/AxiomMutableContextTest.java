82836cb0937c2088e34935065a028d3a2f89502c
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-03-28 14:38:05 +0000

    charge.  start building mutable test suite; enable mutability in axiom bridge.

---
+++ bridge/axiom/src/test/java/org/genxdm/bridge/axiom/tests/AxiomMutableContextTest.java
@@ -0,0 +1,17 @@
+package org.genxdm.bridge.axiom.tests;
+
+import org.apache.axiom.om.impl.llom.factory.OMLinkedListImplFactory;
+import org.genxdm.bridge.axiom.AxiomProcessingContext;
+import org.genxdm.bridgetest.mutable.MutableContextBase;
+
+public class AxiomMutableContextTest
+    extends MutableContextBase<Object>
+{
+
+    @Override
+    public AxiomProcessingContext newProcessingContext()
+    {
+        return new AxiomProcessingContext(new OMLinkedListImplFactory());
+    }
+
+}
