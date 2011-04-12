2c5cc30f8a184086ab9749089030ff6a96d93e37
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-04-12 19:34:41 +0000

    bridge copyright notices for axiom, cx, dom (but not for bridgekit)

--- project/bridge/axiom/src/test/java/org/genxdm/bridge/axiom/tests/InformerTest.java
+++ project/bridge/axiom/src/test/java/org/genxdm/bridge/axiom/tests/InformerTest.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2010-2011 TIBCO Software Inc.
+/*
+ * Copyright (c) 2011 TIBCO Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,6 @@ import org.apache.axiom.om.impl.llom.factory.OMLinkedListImplFactory;
 import org.genxdm.bridge.axiom.AxiomProcessingContext;
 import org.genxdm.bridgetest.nodes.InformerBase;
 
-/**
- * Test Class for Cursor unit tests for the Informer methods for Axiom Tree Model.
- * @author Ashutosh
- *
- */
 public class InformerTest 
 	extends InformerBase<Object>
 {
