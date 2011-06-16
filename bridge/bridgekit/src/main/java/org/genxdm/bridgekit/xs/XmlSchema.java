1986407efdbf09aadc2c9c3530c844ef62c87b85
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-06-16 16:57:28 +0000

    although we've got some more testing to do for schema parsing, it's about time to take it on trust (may be buggy, but base function is working), and move on to the thorny problem of validation. this checkin mostly cleans up the 'builtins' implementation, and adds a bag-to-component provider (already slotted for use in testing) utility. we also reduce the circularity found in bridgekit's schema implementation.

--- bridge/bridgekit/src/main/java/org/genxdm/bridgekit/xs/XmlSchema.java
+++ bridge/bridgekit/src/main/java/org/genxdm/bridgekit/xs/XmlSchema.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) 2009-2010 TIBCO Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,9 +31,7 @@ import org.genxdm.xs.components.NotationDefinition;
 import org.genxdm.xs.constraints.IdentityConstraint;
 import org.genxdm.xs.enums.ScopeExtent;
 import org.genxdm.xs.types.ComplexType;
-import org.genxdm.xs.types.NativeType;
 import org.genxdm.xs.types.SimpleType;
-import org.genxdm.xs.types.Type;
 
 final class XmlSchema implements ComponentBag
 {
@@ -47,44 +45,21 @@ final class XmlSchema implements ComponentBag
     /**
      * Constructs the W3C XML Schema native types and atributes.
      */
-    public XmlSchema(final SchemaCacheImpl cache)
+    public XmlSchema()
     {
         // these are in blocks so that we can call register using a standard pattern.
         {
             final QName name = new QName(XMLConstants.XML_NS_URI, "base", XMLConstants.XML_NS_PREFIX);
-            final SimpleType type = cache.getAtomicType(NativeType.ANY_URI);
+            final SimpleType type = BuiltInSchema.SINGLETON.ANY_URI;
             XML_BASE = new AttributeDeclTypeImpl(name, ScopeExtent.Global, type);
             register(XML_BASE);
         }
         {
             final QName name = new QName(XMLConstants.XML_NS_URI, "lang", XMLConstants.XML_NS_PREFIX);
-            final SimpleType type = cache.getAtomicType(NativeType.LANGUAGE);
+            final SimpleType type = BuiltInSchema.SINGLETON.LANGUAGE;
             XML_LANG = new AttributeDeclTypeImpl(name, ScopeExtent.Global, type);
             register(XML_LANG);
         }
-    }
-
-    public SimpleType getAtomicType(final QName name)
-    {
-        final SimpleType simpleType = getSimpleType(name);
-        if (simpleType.isAtomicType())
-        {
-            return simpleType;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    public AttributeDefinition getAttribute(final QName name)
-    {
-        return m_attributes.get(name);
-    }
-
-    public AttributeGroupDefinition getAttributeGroup(final QName name)
-    {
-        return null;
     }
 
     public Iterable<AttributeGroupDefinition> getAttributeGroups()
@@ -97,19 +72,9 @@ final class XmlSchema implements ComponentBag
         return m_attributes.values();
     }
 
-    public ComplexType getComplexType(final QName name)
-    {
-        return null;
-    }
-
     public Iterable<ComplexType> getComplexTypes()
     {
         return Collections.emptyList();
-    }
-
-    public ElementDefinition getElement(final QName name)
-    {
-        return null;
     }
 
     public Iterable<ElementDefinition> getElements()
@@ -117,19 +82,9 @@ final class XmlSchema implements ComponentBag
         return Collections.emptyList();
     }
 
-    public IdentityConstraint getIdentityConstraint(final QName name)
-    {
-        return null;
-    }
-
     public Iterable<IdentityConstraint> getIdentityConstraints()
     {
         return Collections.emptyList();
-    }
-
-    public ModelGroup getModelGroup(final QName name)
-    {
-        return null;
     }
 
     public Iterable<ModelGroup> getModelGroups()
@@ -137,74 +92,14 @@ final class XmlSchema implements ComponentBag
         return Collections.emptyList();
     }
 
-    public NotationDefinition getNotation(final QName name)
-    {
-        return null;
-    }
-
     public Iterable<NotationDefinition> getNotations()
     {
         return Collections.emptyList();
     }
 
-    public SimpleType getSimpleType(final QName name)
-    {
-        return m_simpleTypes.get(name);
-    }
-
     public Iterable<SimpleType> getSimpleTypes()
     {
         return m_simpleTypes.values();
-    }
-
-    public Type getType(final QName name)
-    {
-        return getSimpleType(name);
-    }
-
-    public boolean hasAttribute(final QName name)
-    {
-        return m_attributes.containsKey(name);
-    }
-
-    public boolean hasAttributeGroup(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasComplexType(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasElement(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasIdentityConstraint(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasModelGroup(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasNotation(final QName name)
-    {
-        return false;
-    }
-
-    public boolean hasSimpleType(final QName name)
-    {
-        return m_simpleTypes.containsKey(name);
-    }
-
-    public boolean hasType(final QName name)
-    {
-        return hasSimpleType(name);
     }
 
     private void register(final AttributeDefinition attribute)
