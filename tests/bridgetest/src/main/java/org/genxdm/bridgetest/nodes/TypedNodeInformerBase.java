99462feb1aa6f264f0e2ee7c7c3b97faa534a998
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-09-08 20:24:23 +0000

    okay. fix up some of the contract javadoc for typednodeinformer. make sure that both cx and dom are conforming to the contract (need tests, kthxbai). start adding test code. verify against (external) xpath2 test code.

---
+++ tests/bridgetest/src/main/java/org/genxdm/bridgetest/nodes/TypedNodeInformerBase.java
@@ -0,0 +1,9 @@
+package org.genxdm.bridgetest.nodes;
+
+import org.genxdm.bridgetest.typed.TypedTestBase;
+
+public abstract class TypedNodeInformerBase<N, A>
+    extends TypedTestBase<N, A>
+{
+
+}
