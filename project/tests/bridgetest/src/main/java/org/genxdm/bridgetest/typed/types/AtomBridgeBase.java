9493facfe2ff4a875ac639b8189b672d5264e6c1
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2011-10-31 16:47:56 +0000

    add a test skeleton for atom bridge; add the proposed javax.xml.transform implementation package.

---
+++ project/tests/bridgetest/src/main/java/org/genxdm/bridgetest/typed/types/AtomBridgeBase.java
@@ -0,0 +1,9 @@
+package org.genxdm.bridgetest.typed.types;
+
+import org.genxdm.bridgetest.typed.TypedTestBase;
+
+public abstract class AtomBridgeBase<N, A>
+    extends TypedTestBase<N, A>
+{
+
+}
