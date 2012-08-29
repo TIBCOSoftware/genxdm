070ddf01928404550fd13d7b550210d602641a22
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2012-08-29 13:58:21 +0000

    remove old trunk (see frozen-trunk on branches) in preparation for moving osgi build into place

--- trunk/project/tests/bridgetest/src/main/java/org/genxdm/bridgetest/typed/types/AtomBridgeBase.java
+++
@@ -1,9 +0,0 @@
-package org.genxdm.bridgetest.typed.types;
-
-import org.genxdm.bridgetest.typed.TypedTestBase;
-
-public abstract class AtomBridgeBase<N, A>
-    extends TypedTestBase<N, A>
-{
-
-}
