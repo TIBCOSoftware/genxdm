999096de35d47c247ee98a603736b985c0aed791
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2012-11-13 18:00:44 +0000

    add typed api support in the axiom bridge. resolves issue 114. Note that this commit is also the cause of issue 115, but life is like that.

---
+++ project/bridge.axiom/src/test/java/org/genxdm/bridge/axiom/tests/typed/POTreeValidationTest.java
@@ -0,0 +1,35 @@
+package org.genxdm.bridge.axiom.tests.typed;
+
+import org.apache.axiom.om.impl.llom.factory.OMLinkedListImplFactory;
+import org.genxdm.bridge.axiom.AxiomProcessingContext;
+import org.genxdm.bridgekit.atoms.XmlAtom;
+import org.genxdm.processor.w3c.xs.validation.ValidatorFactory;
+import org.genxdm.processor.w3c.xs.validationtest.TreeValidationBase;
+import org.genxdm.typed.ValidationHandler;
+import org.genxdm.typed.io.SAXValidator;
+
+public class POTreeValidationTest
+    extends TreeValidationBase<Object, XmlAtom>
+{
+
+    @Override
+    public AxiomProcessingContext newProcessingContext()
+    {
+        return new AxiomProcessingContext(new OMLinkedListImplFactory());
+    }
+
+    @Override
+    public SAXValidator<XmlAtom> getSAXValidator()
+    {
+        ValidatorFactory<Object, XmlAtom> factory = new ValidatorFactory<Object, XmlAtom>(newProcessingContext().getTypedContext(null));
+        return factory.newSAXContentValidator();
+    }
+
+    @Override
+    public ValidationHandler<XmlAtom> getValidationHandler()
+    {
+        ValidatorFactory<Object, XmlAtom> factory = new ValidatorFactory<Object, XmlAtom>(newProcessingContext().getTypedContext(null));
+        return factory.newXdmContentValidator();
+    }
+
+}
---
+++ project/bridgekit/src/main/java/org/genxdm/bridgekit/misc/DefaultMapEntry.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.genxdm.bridgekit.misc;
+
+import java.util.Map;
+
+/**
+ * A restricted implementation of {@link Map.Entry} that prevents
+ * the {@link Map.Entry} contract from being broken.
+ *
+ * @since 3.0
+ * @version $Id: DefaultMapEntry.java 1361689 2012-07-15 11:45:57Z tn $
+ */
+public final class DefaultMapEntry<K, V> extends AbstractMapEntry<K, V> {
+
+    /**
+     * Constructs a new entry with the specified key and given value.
+     *
+     * @param key  the key for the entry, may be null
+     * @param value  the value for the entry, may be null
+     */
+    public DefaultMapEntry(final K key, final V value) {
+        super(key, value);
+    }
+
+    /**
+     * Constructs a new entry from the specified <code>KeyValue</code>.
+     *
+     * @param pair  the pair to copy, must not be null
+     * @throws NullPointerException if the entry is null
+     */
+    public DefaultMapEntry(final KeyValue<K, V> pair) {
+        super(pair.getKey(), pair.getValue());
+    }
+
+    /**
+     * Constructs a new entry from the specified <code>Map.Entry</code>.
+     *
+     * @param entry  the entry to copy, must not be null
+     * @throws NullPointerException if the entry is null
+     */
+    public DefaultMapEntry(final Map.Entry<K, V> entry) {
+        super(entry.getKey(), entry.getValue());
+    }
+
+}
