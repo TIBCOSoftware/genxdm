4f1f6c242f402cda5237655635cbd7ebb418fe6d
aaletal <aaletal@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2013-01-23 17:56:53 +0000

    update the axiom bridge for issue 120; also update FauxElement to allow us to build against axiom 1.2.14 and change the import (but don't just unlock it; they're too willing to break things at random)

--- project/bridgekit/src/main/java/org/genxdm/bridgekit/tree/TypeAnnotator.java
+++ project/bridgekit/src/main/java/org/genxdm/bridgekit/tree/TypeAnnotator.java
@@ -4,5 +4,10 @@ import javax.xml.namespace.QName;
 
 public interface TypeAnnotator
 {
+    /** Associate a node Id with a type name.
+     * 
+     * @param nodeId The node ID (from Model.getNodeId(node); may not be null.
+     * @param typeName The type name.
+     */
     void annotate(Object nodeId, QName typeName);
 }
