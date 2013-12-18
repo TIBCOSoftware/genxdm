9c0ba88cceae7720290ba205af442ca55ad0100f
joe.baysdon@gmail.com <joe.baysdon@gmail.com@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2013-12-18 13:48:49 +0000

    (Issue 148) Add OPTION_LAST_IN_WINS to LoadOptions; support that option in the parser.

--- processor.w3c.xs/src/main/java/org/genxdm/processor/w3c/xs/LoadOptions.java
+++ processor.w3c.xs/src/main/java/org/genxdm/processor/w3c/xs/LoadOptions.java
@@ -8,7 +8,11 @@ public interface LoadOptions
     
     static final String PROCESS_REPEATED_NAMESPACES = "processRepeatedNamespaces";
     
+    static final String LAST_IN_WINS = "lastInWins";
+    
     static final QName OPTION_REPEAT_NS = new QName(W3C_XS_PROCESSOR_NS, PROCESS_REPEATED_NAMESPACES);
+    
+    static final QName OPTION_LAST_IN_WINS = new QName(W3C_XS_PROCESSOR_NS, LAST_IN_WINS);
     
     // in this parser, boolean options that have the string value "true" or "yes"
     // are set true; boolean options with *any other value* are considered false. 
