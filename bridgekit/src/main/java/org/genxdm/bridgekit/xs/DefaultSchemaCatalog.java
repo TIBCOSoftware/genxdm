a877cfaa4a16b77bd568b0ce20d007f85ea73ea5
aaletal@gmail.com <aaletal@gmail.com@21dcfcec-52f6-7d45-41bb-77dfb31e8e87>
2014-09-17 13:19:10 +0000

    missed checkin, apparently. causes problems.

--- bridgekit/src/main/java/org/genxdm/bridgekit/xs/DefaultSchemaCatalog.java
+++ bridgekit/src/main/java/org/genxdm/bridgekit/xs/DefaultSchemaCatalog.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.genxdm.processor.w3c.xs;
+package org.genxdm.bridgekit.xs;
 
 import java.net.URI;
 
