4aa882707d1c0a36dc6211357a999b7249152ec1
aaletal <aaletal@gmail.com>
2015-05-12 17:07:50 +0000

    update stale validation test changes to depend on bridgekit default catalog/resolver tools

--- project/w3c.xs.validationtest/src/main/java/org/genxdm/processor/w3c/xs/validationtest/TreeValidationBase.java
+++ project/w3c.xs.validationtest/src/main/java/org/genxdm/processor/w3c/xs/validationtest/TreeValidationBase.java
@@ -8,12 +8,12 @@ import java.io.InputStream;
 import java.net.URI;
 
 import org.genxdm.ProcessingContext;
+import org.genxdm.bridgekit.xs.DefaultCatalog;
+import org.genxdm.bridgekit.xs.DefaultCatalogResolver;
+import org.genxdm.bridgekit.xs.DefaultSchemaCatalog;
 import org.genxdm.bridgekit.xs.SchemaCacheFactory;
 import org.genxdm.io.DocumentHandler;
 import org.genxdm.names.Catalog;
-import org.genxdm.processor.w3c.xs.DefaultCatalog;
-import org.genxdm.processor.w3c.xs.DefaultCatalogResolver;
-import org.genxdm.processor.w3c.xs.DefaultSchemaCatalog;
 import org.genxdm.processor.w3c.xs.W3cXmlSchemaParser;
 import org.genxdm.typed.TypedContext;
 import org.genxdm.typed.ValidationHandler;
